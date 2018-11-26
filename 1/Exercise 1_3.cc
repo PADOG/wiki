2ada636754e96ddcc6587f69af093deceec9a2ce
OvO <Ricardo.M.Jia@gmail.com>
2018-11-26 21:31:16 +0800

    c++ primer exercise

--- 1/Exercise 1_3.cc
+++ 1/Exercise 1_3.cc
@@ -3,13 +3,11 @@
  *
  * 练习 1.3：
  * 编写程序，在标准输出上打印 Hello, World。
- *
- */
+*/
 
 #include <iostream>
 
-int mian()
-{
-    std::cout << "Hello, World" << std::endl;
+int main() {
+    std::cout << "Hello, World。" << std::endl;
     return 0;
 }
--- 1/Exercise 1_4.cc
+++ 1/Exercise 1_4.cc
@@ -7,12 +7,11 @@
 
 #include <iostream>
 
-int main()
-{
+int main() {
     std::cout << "Enter two number: " << std::endl;
     int v1 = 0, v2 = 0;
     std::cin >> v1 >> v2;
-    std::cout << "The mul of " << v1 << " and " << v2 
-              << " is " << v1 * v2 << std:endl;
+    std::cout << "The mul of " << v1 << " and " << v2
+              << " is " << v1 * v2 << std::endl;
     return 0;
 }
